bfe1bfff6eb6a565be8211e7cae2e152cd44a516
Pedro "Coin" Duarte <coinbr@gmail.com>
2019-02-13 14:41:24 -0300

    pack.sh: Fix twine install cmd

--- pack.sh
+++ pack.sh
@@ -1,12 +1,14 @@
+localPath=$PWD
 i="sudo apt get update && sudo apt-get install"
 
+cd $localPath
 # Install dependencies:
 
 	which pip3 || $i python3-pip &&
 
 	sudo python3 -m pip install --upgrade pip setuptools wheel &&
 	sudo python3 -m pip install tqdm &&
-	sudo python3 -m pip install --user --upgrade twine &&
+	sudo python3 -m pip install --upgrade twine &&
 
 
 # Compile Package:
