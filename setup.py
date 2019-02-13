acc1c1459ab57ad25a6737ade3f8a1b8c0d38ea0
Pedro "Coin" Duarte <coinbr@gmail.com>
2019-02-13 13:47:34 -0300

    Pack the module

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='physh',  
+    version='0.0.1',
+    # scripts=['physh'] ,
+    # packages=['physh'],
+    author="Pedro 'Coin' Duarte",
+    author_email="coinbr@gmail.com",
+    description="run shell commands with this thin wrapper around the subprocess module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CoinBR/physh/tree/master",
+    packages=setuptools.find_packages(),
+    classifiers=[
+     "Programming Language :: Python :: 3.6",
+     "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+     "Operating System :: POSIX :: Linux",
+    ],
+ )
